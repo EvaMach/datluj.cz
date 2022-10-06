2c15ef6e26a13f9668036fc2fc3c6d80489ddde0
Eva Machova <evca.machova@gmail.com>
2022-10-06 21:29:37 +0200

    create game

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -4,27 +4,42 @@ import wordList from '../../word-list';
 import './style.css';
 
 const generateWord = (size) => {
-  const sizeIndex = size === undefined
-    ? Math.floor(Math.random() * wordList.length)
-    : size - 3;
-  
+  const sizeIndex =
+    size === undefined ? Math.floor(Math.random() * wordList.length) : size - 3;
+
   if (sizeIndex < 0 || sizeIndex >= wordList.length) {
     return null;
   }
-  
+
   const words = wordList[sizeIndex];
   const wordIndex = Math.floor(Math.random() * words.length);
   return words[wordIndex];
 };
 
 const Stage = () => {
-  const [words, setWords] = useState(['jahoda']);
+  const [words, setWords] = useState(['jahoda', 'lahoda', 'náhoda']);
+  const [mistakeCount, setMistakeCount] = useState(0);
+
+  const handleFinish = () => {
+    const newWord = generateWord(6);
+    setWords([...words.slice(1), newWord]);
+  };
 
   return (
     <div className="stage">
-      <div className="stage__mistakes">Chyb: 0</div>
+      <div className="stage__mistakes">Chyb: {mistakeCount}</div>
       <div className="stage__words">
-        {words.map((word) => <Wordbox word={word} key={word} />)}
+        {words.map((word, index) => (
+          <>
+            <Wordbox
+              active={index === 0 ? true : false}
+              onFinish={handleFinish}
+              onMistake={setMistakeCount}
+              word={word}
+              key={word}
+            />
+          </>
+        ))}
       </div>
     </div>
   );
--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
+const Wordbox = ({ word, onFinish, onMistake, active }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);
+  const [mistake, setMistake] = useState(false);
+
+  const handleKeyUp = (e) => {
+    if (e.key === lettersLeft[0]) {
+      setMistake(false);
+      if (lettersLeft.length > 1) {
+        setLettersLeft(lettersLeft.slice(1));
+      } else {
+        onFinish();
+      }
+    } else {
+      setMistake(true);
+      onMistake((mistake) => mistake + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (active) {
+      document.addEventListener('keyup', handleKeyUp);
+    }
+    if (active) {
+      return () => document.removeEventListener('keyup', handleKeyUp);
+    }
+  }, [lettersLeft, active]);
+
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={`wordbox ${mistake && 'wordbox--mistake'}`}>
+      {lettersLeft}
+    </div>
   );
 };
 
